aa4bf6fa62d3519eab8623a165f02f5b9dad8778
Doger Token <80608747+dogertoken@users.noreply.github.com>
2025-03-12 08:01:57 +0700

    Update HKContractV2.sol

--- HKContractV2.sol
+++ HKContractV2.sol
@@ -36,7 +36,7 @@ contract LottoChainDecentralizedHKPool {
 
     constructor(address _usdt) {
         owner = msg.sender;
-        USDT = IERC20(_usdt);
+        USDT = IERC20(0xdAC17F958D2ee523a2206206994597C13D831ec7);
     }
 
     modifier onlyOwner() {
