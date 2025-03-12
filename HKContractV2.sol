d807889995704efe492ecd6842ac21652ba77990
Doger Token <80608747+dogertoken@users.noreply.github.com>
2025-03-12 07:56:41 +0700

    Update HKContractV2.sol

--- HKContractV2.sol
+++ HKContractV2.sol
@@ -1,7 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+interface IERC20 {
+    function transfer(address recipient, uint256 amount) external returns (bool);
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+    function balanceOf(address account) external view returns (uint256);
+}
 
 contract LottoChainDecentralizedHKPool {
     address public owner;
